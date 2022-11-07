33719aedac14e42ee0f25ea253e4e800ced5ef70
Elie Parthenay <elie.parthenay@gmail.com>
2022-11-07 11:38:40 +0100

    express 05

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", userHandlers.updateUsers);
 
+// DELETE
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUsers);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
--- userHandlers.js
+++ userHandlers.js
@@ -69,9 +69,28 @@ const updateUsers = (req, res) => {
         });
 };
 
+const deleteUsers = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+        .query("delete from users where id = ?", [id])
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error deleting the user");
+        });
+};
+
 module.exports = {
     getUsers,
     getUserById,
     postUsers,
     updateUsers,
+    deleteUsers,
 };
